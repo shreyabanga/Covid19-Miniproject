8221f1bcdfbe34b05c9cdd624297beeff0dba1bd
esbirol <45472898+esbirol@users.noreply.github.com>
2020-09-09 14:28:49 -0400

    somethings added

--- covid19_tracker/README.md
+++ covid19_tracker/README.md
@@ -1,6 +1,6 @@
-# covid19_tracker
+# rest_api
 
-A new Flutter project.
+A new Flutter application.
 
 ## Getting Started
 
--- covid19_tracker/lib/main.dart
+++ covid19_tracker/lib/main.dart
@@ -1,32 +1,19 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-import 'login.dart';
+import 'package:rest_api/views/note_list.dart';
 
-void main() => runApp(MyApp());
-
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Covid-19 Tracker',
-      initialRoute: '/',
-          routes: {
-            '/': (context) => new Login(),
-            // '/LogIn': (BuildContext context) => new LoginPage(),
-            // '/Redirect': (BuildContext context) => new Redirect(auth: new Auth()),
-            // '/SignUp': (BuildContext context) => new InputForm(),
-            // '/Form': (BuildContext context) => new InputForm(),
-            // '/UploadPet': (BuildContext context) => new UploadPet(),
-            //// '/Org_Profile': (BuildContext context) => new OrgProfile(),
-            // '/Edit_Pet_Profile': (BuildContext context) => new EditPetProfile(),
-            // '/User_Profile' : (BuildContext context) => new UserProfile(),
-            //'/': (context) => OrgProfile(),
-          }
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: NoteList(),
     );
   }
 }
